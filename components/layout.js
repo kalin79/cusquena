981e41c39e8711ba0432d1fda8d93df7262636a2
Carlos Espinoza <c.augusto.espinoza@gmail.com>
2023-07-02 23:01:20 -0500

    meta agregados, el de las imagenes comentado

--- components/layout.js
+++ components/layout.js
@@ -8,6 +8,19 @@ export default function Layout({children,title='',description=''}) {
                     <title>{title}</title>
                     <meta name="title" content={title} />
                     <meta name="description" content={description} />
+                    {/* <meta property="og:image" content="<generated>" />
+                    <meta property="og:image:type" content="<generated>" />
+                    <meta property="og:image:width" content="<generated>" />
+                    <meta property="og:image:height" content="<generated>" /> */}
+                    
+                    {/* <meta name="twitter:image" content="<generated>" />
+                    <meta name="twitter:image:type" content="<generated>" />
+                    <meta name="twitter:image:width" content="<generated>" />
+                    <meta name="twitter:image:height" content="<generated>" /> */}
+
+                    <meta property="og:image:alt" content="Cusqueña, Maestros del Sabor" />
+
+                    <meta name="robots" content="index,follow"/>
                     {/* <meta
                          http-equiv="Content-Security-Policy"
                          content="default-src 'self'; img-src https://*; child-src 'none';" 
